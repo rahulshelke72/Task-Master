ee8d315cce1e13787f741de36e55e42ba8440351
Rahul <rahulshelke4172@gmail.com>
2023-11-17 20:50:10 +0530

    add task activity , home page

--- app/src/main/java/com/example/taskmanager/HomeActivity.java
+++ app/src/main/java/com/example/taskmanager/HomeActivity.java
@@ -1,14 +1,185 @@
 package com.example.taskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.example.taskmanager.model.TaskModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+//import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//import de.hdodenhof.circleimageview.CircleImageView;
+//import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class HomeActivity extends AppCompatActivity {
 
+    RecyclerView taskRv;
+    ArrayList<TaskModel> dataList=new ArrayList<>();
+    TaskListAdapter taskListAdapter;
+    FirebaseFirestore db;
+    String TAG="Homepage query docs";
+    TextView userNameTv;
+//    CircleImageView userImageIv;
+    SearchView searchView;
+//    @Override
+//    protected void attachBaseContext(Context newBase) {
+//        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+
+        db=FirebaseFirestore.getInstance();
+        taskRv=findViewById(R.id.taskListRv);
+        userNameTv=findViewById(R.id.userNameTv);
+//        userImageIv=findViewById(R.id.userProfileIv);
+        searchView=findViewById(R.id.searchview);
+
+
+//
+//        userImageIv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(HomeActivity.this,SettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
+        userNameTv.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+
+//        Picasso.get().load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(userImageIv);
+
+
+
+
+
+        taskListAdapter=new TaskListAdapter(dataList);
+
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        taskRv.setLayoutManager(layoutManager);
+        taskRv.setAdapter(taskListAdapter);
+
+
+        findViewById(R.id.addTaskFAB).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,AddTaskActivity.class));
+            }
+        });
+
+
+        db.collection("tasks")
+                .whereEqualTo("userId", FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+
+
+                                TaskModel taskModel= document.toObject(TaskModel.class);
+                                taskModel.setTaskId(document.getId());
+
+                                dataList.add(taskModel);
+                                taskListAdapter.notifyDataSetChanged();
+
+
+
+
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+
+
+//test
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d("search"," "+s);
+                taskListAdapter.clearAllItems();
+                db.collection("tasks")
+                        .orderBy("taskName")
+                        .startAt(s).
+                        endAt(s+'\uf8ff')
+
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+
+
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.getData());
+
+
+                                        TaskModel taskModel= document.toObject(TaskModel.class);
+                                        taskModel.setTaskId(document.getId());
+
+                                        dataList.add(taskModel);
+                                        taskListAdapter.notifyDataSetChanged();
+
+
+
+
+
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+
+
+                return false;
+            }
+        });
+
+
+
+
+
+
+
     }
 }
